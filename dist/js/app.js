ba79f28bee321f13da86500687f00e18863df83d
Олег <nexxxt.07@gmail.com>
2022-10-10 00:33:06 +0300

    09.10 2

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,27 @@
-import '~/node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.getElementById('teamsSlider')) {
+        const sliderBrands = new Swiper('#teamsSlider', {
+            slidesPerView: 4,
+           /* navigation: {
+                nextEl: '.button-next',
+                prevEl: '.button-prev',
+            },*/
+            breakpoints: {
+                768: {
+                    slidesPerView: 3,
+                },
+                991: {
+                    slidesPerView: 4,
+                },
+                1200: {
+                    slidesPerView: 4,
+                },
+            },
+            on: {
+                init: function (e) {
 
-
-
+                }
+            }
+        });
+    }
+});
